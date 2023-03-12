f188e6bdd7c436263b35a73ad4d811cfeecd68fd
Heather Storey <heathernstorey@gmail.com>
2023-03-11 22:54:10 -0500

    Codewars added

--- homework2.js
+++ homework2.js
@@ -113,3 +113,42 @@ isBigWord("asynchronous")
 .catch( (reject) => {
     console.log(`Small word`)
 })
+
+// CODEWARS SOLUTIONS //
+/**
+ * DESCRIPTION:
+Complete the solution so that it reverses the string passed into it.
+
+PYTHON SOLUTION:
+def solution(string):
+    return string[::-1]
+
+JAVASCRIPT SOLUTION:
+*/
+function solution(str){
+    return str.split("").reverse().join("");
+}
+
+// CODEWARS SOLUTION #1
+// Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+
+// []                                -->  "no one likes this"
+// ["Peter"]                         -->  "Peter likes this"
+// ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+// ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+// ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+
+// SOLUTION:
+function likes(names) {
+    if(names.length == 0) {
+      return "no one likes this"
+    } else if(names.length == 1) {
+      return `${names} likes this`
+    } else if(names.length == 2) {
+      return `${names[0]} and ${names[1]} like this`
+    } else if(names.length == 3) {
+      return `${names[0]}, ${names[1]} and ${names[2]} like this`
+    } else {
+      return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
+    }
+  }
